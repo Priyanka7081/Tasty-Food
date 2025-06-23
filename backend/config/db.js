947c179e764ccd9671ffc37886da2b40c9bf0ae9
Priyanka7081 <priyankau7081@gmail.com>
2025-06-23 18:39:22 +0530

    done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://um8794907:uttam262903@cluster0.54lnu.mongodb.net/restuarant')
+    await mongoose.connect('mongodb+srv://priyankau7081:Priya1234@cluster0.x7jkev7.mongodb.net/tastyfood')
     .then(()=>{
         console.log('mongodb is connected...')
     }).catch(err =>console.log("err"));
--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,24 +1,6 @@
 import userModel from './../models/userModel.js';
 
-// const addToCart = async (req, res) => {
-//     try {
-//         let userData = await userModel.findById(req.body.userId);
-//         let cartData = userData;
 
-//         if (!cartData[req.body.itemId]) {
-//             cartData[req.body.itemId] = 1;
-//         } else {
-//             cartData[req.body.itemId] += 1;
-//         }
-
-//         await userModel.findByIdAndUpdate(req.body.userId, { cartData });
-
-//         res.json({ success: true, message: "Added to Cart" });
-//     } catch (error) {
-//         console.log(error);
-//         res.json({ success: false, message: "Error" });
-//     }
-// };
 
 const addToCart = async (req, res) => {
     try {
